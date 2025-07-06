4476b6b20a9f92b275f018a84f4ae8b7adc50e51
Meredith Berenson <meredithberenson@Merediths-Air.attlocal.net>
2025-07-06 16:45:04 -0400

    Add PrivateRoute component and update App.js for protected routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,30 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import Dashboard from './pages/Dashboard';
 import LoginPage from './pages/LoginPage';
 import ErrorPage from './pages/ErrorPage';
+import PrivateRoute from './components/PrivateRoute';
+import Navbar from './components/Navbar';
 import ScheduleView from './pages/ScheduleView';
-import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/schedule" element={<ScheduleView />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/schedule" element={<PrivateRoute><ScheduleView /></PrivateRoute>} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
   );
 }
 
