a5303225068fff9f866907b153cf576797d92c15
Meredith Berenson <meredithberenson@Merediths-Air.attlocal.net>
2025-07-06 16:37:10 -0400

    Add ScheduleView route and finalize App routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Navbar from './components/Navbar';
 import Dashboard from './pages/Dashboard';
 import LoginPage from './pages/LoginPage';
 import ErrorPage from './pages/ErrorPage';
 import ScheduleView from './pages/ScheduleView';
+import { AuthProvider } from './context/AuthContext';
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
+    <AuthProvider>
+      <Router>
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/schedule" element={<ScheduleView />} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
-      </div>
-    </Router>
+      </Router>
+    </AuthProvider>
   );
-};
+}
 
 export default App;
 
