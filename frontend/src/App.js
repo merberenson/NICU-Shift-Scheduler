d3a9f6275ec0af8adda26ebe93a2dec6f5541e9f
simandjang <simandjang@gmail.com>
2025-07-11 20:15:27 -0400

    Removing error throwing code.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import LoginPage from './pages/LoginPage';
+import AdminMain from './pages/AdminMain';
+import NurseMain from './pages/NurseMain';
 import ErrorPage from './pages/ErrorPage';
-import PrivateRoute from './components/PrivateRoute';
-import Navbar from './components/Navbar';
-import ScheduleView from './pages/ScheduleView';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route
-          path="/"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
-        <Route path="/schedule" element={<PrivateRoute><ScheduleView /></PrivateRoute>} />
+        {/* <Route path="/login" element={<LoginPage />} /> */}
         <Route path="*" element={<ErrorPage />} />
+        <Route path="/schedule" element={<AdminMain />} />
+        <Route path="/main" element={<NurseMain />} />
       </Routes>
     </Router>
   );
